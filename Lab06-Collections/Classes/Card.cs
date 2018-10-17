ec6661ff02443953a1b3e673e2741eb1e3270c00
rebeccahong <rebeccayhong@gmail.com>
2018-10-17 10:54:04 -0700

    Add test project

---
+++ Lab06-Collections/Classes/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Create a custom generic collection named Deck<T>.
+//Your Generic collection should hold Cards. (You will need to create a custom Card class)
+//Create an Enum to hold the different card suits(Hearts, Diamonds, Spades, Clubs)
+//The methods within your Deck class should contain at minimum:
+//Add
+//Remove
+//ReturnSuit
+//This method will return all of the cards in that suit within the deck.
+
+namespace Lab06_Collections.Classes
+{
+    public class Card
+    {
+    }
+}
---
+++ Lab06_Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+//Tests
+//Your tests should cover the following functionality:
+
+//Add a card to your deck
+//Getter/Setters of your properties from your Card class
+//Remove a card from your deck that exists
+//Remove a card from your deck that does not exist
+
+namespace Lab06_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
